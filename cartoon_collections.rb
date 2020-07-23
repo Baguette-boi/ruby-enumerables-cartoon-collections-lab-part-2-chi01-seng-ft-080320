065a2657296b73e7ca4ad60b187c2926b071c36e
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-23 19:52:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  planeteer_calls.find do |string| string.class = valid_calls || string == nil
+  planeteer_calls.find do |string| valid_calls.include?(string)
 end
 
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
